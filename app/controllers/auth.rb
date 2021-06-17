6c092162e41c0e792e28f8690cafb31f4aedda88
Soumya Ray <soumya.ray@gmail.com>
2021-06-17 16:21:53 +0800

    Requres signed requests for non-authenticated routes

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -7,11 +7,17 @@ module Credence
   # Web controller for Credence API
   class Api < Roda
     route('auth') do |routing| # rubocop:disable Metrics/BlockLength
+      # All requests in this route require signed requests
+      begin
+        @request_data = SignedRequest.new(Api.config).parse(request.body.read)
+      rescue SignedRequest::VerificationError
+        routing.halt '403', { message: 'Must sign request' }.to_json
+      end
+
       routing.on 'register' do
         # POST api/v1/auth/register
         routing.post do
-          reg_data = JsonRequestBody.parse_symbolize(request.body.read)
-          VerifyRegistration.new(reg_data).call
+          VerifyRegistration.new(@request_data).call
 
           response.status = 202
           { message: 'Verification email sent' }.to_json
@@ -28,8 +34,7 @@ module Credence
       routing.is 'authenticate' do
         # POST /api/v1/auth/authenticate
         routing.post do
-          credentials = JsonRequestBody.parse_symbolize(request.body.read)
-          auth_account = AuthenticateAccount.call(credentials)
+          auth_account = AuthenticateAccount.call(@request_data)
           { data: auth_account }.to_json
         rescue AuthenticateAccount::UnauthorizedError
           routing.halt '401', { message: 'Invalid credentials' }.to_json
@@ -38,9 +43,7 @@ module Credence
 
       # POST /api/v1/auth/sso
       routing.post 'sso' do
-        auth_request = JsonRequestBody.parse_symbolize(request.body.read)
-
-        auth_account = AuthorizeSso.new.call(auth_request[:access_token])
+        auth_account = AuthorizeSso.new.call(@request_data[:access_token])
         { data: auth_account }.to_json
       rescue StandardError => error
         puts "FAILED to validate Github account: #{error.inspect}"
